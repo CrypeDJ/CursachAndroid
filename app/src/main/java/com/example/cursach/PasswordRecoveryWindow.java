9e5fd623b4cc5610e9ee9259d6d4e4ca2dbb07e6
CrypeDJ <litowrog@gmail.com>
2023-10-14 15:59:01 +0300

    TestChangePass#1

--- app/src/main/java/com/example/cursach/PasswordRecoveryWindow.java
+++ app/src/main/java/com/example/cursach/PasswordRecoveryWindow.java
@@ -15,12 +15,16 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.example.cursach.models.Validation;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.IOException;
 
 public class PasswordRecoveryWindow extends AppCompatActivity {
 
@@ -40,10 +44,10 @@ public class PasswordRecoveryWindow extends AppCompatActivity {
         recoveryWindow = findViewById(R.id.recovery_window);
         btnSendMessage = findViewById(R.id.btn_send_recovery);
 
+        progressDialog = new ProgressDialog(this);
         btnSendMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                progressDialog.show();
                 sendMessage(view);
             }
         });
@@ -51,26 +55,34 @@ public class PasswordRecoveryWindow extends AppCompatActivity {
 
 
     public void sendMessage(View v){
-
         if (Validation.isValidEmail(emailToSend.getText().toString())){
-            reg.sendPasswordResetEmail(emailToSend.getText().toString())
-                            .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void unused) {
-                                    progressDialog.cancel();
-                                    Snackbar.make(recoveryWindow, "Код отправлен на почту", Snackbar.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(PasswordRecoveryWindow.this, MainActivity.class);
-                                    startActivity(intent);
-                                }
-                            }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            progressDialog.cancel();
-                            Snackbar.make(recoveryWindow, "Ошибка." + e.getMessage(), Snackbar.LENGTH_SHORT).show();
-                        }
-                    });
-
+            Snackbar.make(recoveryWindow, "Введите email", Snackbar.LENGTH_SHORT).show();
+            return;
         }
 
+        try {
+            progressDialog.show();
+                reg.sendPasswordResetEmail(emailToSend.getText().toString().trim())
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                progressDialog.cancel();
+                                Snackbar.make(recoveryWindow, "Код отправлен на почту", Snackbar.LENGTH_SHORT).show();
+                                Intent intent = new Intent(PasswordRecoveryWindow.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                progressDialog.cancel();
+                                Snackbar.make(recoveryWindow, "Ошибка." + e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                            }
+                        });
+
+
+
+        } catch (Exception e) {
+            progressDialog.cancel();
+            Snackbar.make(recoveryWindow, e.getMessage(),Snackbar.LENGTH_SHORT).show();}
     }
 }
